80752f200f405fd7173c66e9aee53088d5f9dfae
BogdanR6 <bogdanrares732@gmail.com>
2025-03-02 15:10:27 +0000

    Make a few changes to the way plugins are set up - All the plugins from 'plugins' are now required without needing to require each - The themes/colorschemes now live in the 'colorscheme.lua' - Changed the theme to Kanagawa

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -9,6 +9,9 @@
 return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
+
+	enabled = false, -- Disabled
+
   -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
