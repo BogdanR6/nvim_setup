a4f23ab2bf5d71d5b29e35e78d45b480540235f7
BogdanR6 <bogdanrares732@gmail.com>
2025-04-16 20:13:59 +0300

    Add rust support and extra plugins

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -24,9 +24,15 @@ return {
 			}, -- NOTE: Must be loaded before dependants
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
-
+			opts = {
+				servers = {
+					bacon_ls = {
+						enabled = diagnostics == "bacon-ls",
+					},
+					rust_analyzer = { enabled = false },
+				},
+			},
 			-- Useful status updates for LSP.
-			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
 			{ "j-hui/fidget.nvim", opts = {} },
 
 			-- Allows extra capabilities provided by nvim-cmp
